8dec995694b63b7961c06d237c31770cec0bda9e
Quan Ta <quan.ta@lin-digital.com>
2015-05-02 11:17:41 -0700

    add init for haskell mode that do not load much of anything else

---
+++ init-haskell.el
@@ -0,0 +1,46 @@
+
+;; Set the user-emacs-directory if this is not the default emacs config
+(setq user-emacs-directory (expand-file-name "~/clj-emacs/"))
+(add-to-list 'load-path user-emacs-directory)
+(load "init-cask.el")
+
+;; I am sick and tired of 'package! Always mess up at the wrong moment.
+;; Use Cask! once the package is installed, it's there until I fancy
+;; another update/upgrade.
+(add-to-list 'load-path (concat user-emacs-directory "common"))
+(load "setup-evil.el")
+(load "setup-magit.el")
+
+
+;;;;
+;; Customization
+;;;;
+
+;; Add a directory to our load path so that when you `load` things
+;; below, Emacs knows where to look for the corresponding file.
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+
+;; Sets up exec-path-from-shell so that Emacs will use the correct
+;; environment variables
+(load "shell-integration.el")
+
+;; These customizations make it easier for you to navigate files,
+;; switch buffers, and choose options from the minibuffer.
+(load "navigation.el")
+
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+(load "ui.el")
+
+;; These customizations make editing a bit nicer.
+(load "editing.el")
+
+;; For editing lisps
+(load "elisp-editing.el")
+
+;; Hard-to-categorize customizations
+(load "misc.el")
+
+;; Langauage-specific
+(load "setup-haskell.el")
+
