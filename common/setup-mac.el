d33c30fc6b030e328ad76f3f47ce196d6ac28d07
Quan Ta <quan.ta@lin-digital.com>
2015-05-12 17:02:52 -0700

    Make right control hyper and right command meta.

--- common/setup-mac.el
+++ common/setup-mac.el
@@ -3,7 +3,7 @@
 
 ;; Command Keys
 (setq mac-command-modifier 'super)
-(setq mac-right-command-modifier 'hyper)
+(setq mac-right-command-modifier 'meta)
 
 ;; Option Keys
 (setq mac-option-modifier 'meta)
@@ -11,7 +11,7 @@
 
 ;; FN Keys and right Ctrl
 (setq ns-function-modifier 'hyper)
-(setq mac-right-control-modifier 'super)
+(setq mac-right-control-modifier 'hyper)
 
 (defvar normal-frame-alist
   `((font . "Monaco-18")
@@ -27,9 +27,15 @@
 (setq initial-frame-alist
       (copy-alist normal-frame-alist))
 
+;; keybinding to toggle full screen mode
+;; (global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
+
 ;; Move to trash when deleting stuff
 (setq delete-by-moving-to-trash t
       trash-directory "~/.Trash/emacs")
+
+;; Ignore .DS_Store files with ido mode
+;; (add-to-list 'ido-ignore-files "\\.DS_Store")
 
 ;; Don't open files from the workspace in a new frame
 (setq ns-pop-up-frames nil)
